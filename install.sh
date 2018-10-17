7b50fc299c405b7ade65ba70b9b310b5f9559ea8
timber3252 <timber3252@yandex.com>
2018-10-17 18:03:48 +0800

    initial sync

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+ln -sf ~/Dotfiles/xinitrc ~/.xinitrc
+ln -sfd ~/Dotfiles/config/dunst ~/.config/
+ln -sfd ~/Dotfiles/config/i3 ~/.config/
+ln -sfd ~/Dotfiles/config/polybar ~/.config/
+ln -sf ~/Dotfiles/config/compton.conf ~/.config/compton.conf
