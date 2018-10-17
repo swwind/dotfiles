1885d784f46aed87ff8ee52260e364eb84d88e83
timber3252 <timber3252@yandex.com>
2018-10-17 19:02:10 +0800

    first move to github

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # timber3252's Dotfiles
 
 These are the basic dotfiles that I start with when I set up a new environment.
+
+## Installation
+
+For **Arch Linux** users, you can just do as the following, 
+
+```bash
+git clone https://github.com/timber3252/Dotfiles.git
+cd Dotfiles
+./install.sh
+```
+
--- install.sh
+++ install.sh
@@ -1,6 +1,49 @@
-#!/bin/zsh
-ln -sf ~/Dotfiles/xinitrc ~/.xinitrc
-ln -sfd ~/Dotfiles/config/dunst ~/.config/
-ln -sfd ~/Dotfiles/config/i3 ~/.config/
-ln -sfd ~/Dotfiles/config/polybar ~/.config/
-ln -sf ~/Dotfiles/config/compton.conf ~/.config/compton.conf
+#!/bin/bash
+# 
+# global variable
+workspace=$(pwd)
+
+# initially install
+sudo pacman -S xorg-xinit i3-gaps polybar dunst fcitx fcitx-rime compton curl \
+  zsh
+
+if [ $? -ne 0 ]; then
+  return 1
+fi
+
+# oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+if [ $? -ne 0 ]; then
+  return 1
+fi
+if [ -f $workspace/.zshrc ]; then
+  ln -sf $workspace/.zshrc ~/.zshrc
+fi
+
+# configure xinitrc
+if [ -f $workspace/.xinitrc ]; then
+  ln -sf $workspace/.xinitrc ~/.xinitrc
+fi
+
+# i3wm
+if [ -f $workspace/.config/i3/config ]; then
+  ln -sfd $workspace/.config/i3 ~/.config/i3
+fi
+
+# polybar
+if [ -f $workspace/.config/polybar/config -a -f $workspace/.config/polybar/launch_polybar ]; then
+  ln -sfd $workspace/.config/polybar ~/.config/polybar
+fi
+
+# compton
+if [ -f $workspace/.config/compton.conf ]; then
+  ln -sf $workspace/.config/compton.conf ~/.config/compton.conf
+fi
+
+# dunst
+if [ -f $workspace/.config/dunst/dunstrc ]; then
+  ln -sfd $workspace/.config/dunst ~/.config/dunst
+fi
+
